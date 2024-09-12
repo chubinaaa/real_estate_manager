c0a25fa97f3dd55d7688750c0cb489999f546cc4
Luka Chubinidze <chubinidzel08@gmail.com>
2024-09-12 14:19:34 +0400

    add active filters tag UI & X close button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,21 @@
 "use client";
 
-import { useState } from "react";
-import DropDownButton from "../components/ui/buttons/DropDownButton";
+import FilterTag from "../components/ui/tags/FilterTag";
 
 export default function Home() {
-  const [modal1, setModal1] = useState(false);
-  const [modal2, setModal2] = useState(false);
-  const [modal3, setModal3] = useState(false);
-
   return (
     <div className="w-full flex justify-center items-center gap-10 p-10">
-      <DropDownButton
-        label="პირველი"
-        action={() => setModal1((prev) => !prev)}
-        modalState={modal1}
+      <FilterTag
+        label="თბილისი"
+        deleteAct={() => console.log("deleting თბილისი")}
       />
-      <DropDownButton
-        label="მეორე"
-        action={() => setModal2((prev) => !prev)}
-        modalState={modal2}
+      <FilterTag
+        label="1000₾ - 150000₾"
+        deleteAct={() => console.log("deleting ფასი")}
       />
-      <DropDownButton
-        label="მესამე"
-        action={() => setModal3((prev) => !prev)}
-        modalState={modal3}
+      <FilterTag
+        label="55 მ2 - 90 მ2"
+        deleteAct={() => console.log("deleting ზომა")}
       />
     </div>
   );
