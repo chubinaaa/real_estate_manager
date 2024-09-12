88c52287bae9306c3481278a3ce36311105822da
Luka Chubinidze <chubinidzel08@gmail.com>
2024-09-12 15:02:49 +0400

    imported specific icons; change layout metadata; add typescript folder; finished creating reusable components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,7 @@
-import CheckBoxInput from "../components/ui/inputs/CheckBoxInput";
-
 export default function Home() {
   return (
     <div className="w-full flex justify-center items-center gap-10 p-10">
-      <CheckBoxInput
-        label="რეგიონის მიხედვით"
-        id="ტესტი"
-        name="checkboxGroup"
-        value="region"
-      />
+      <h1>Home Page</h1>
     </div>
   );
 }
