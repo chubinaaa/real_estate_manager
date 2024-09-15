0d6d455a3b2a39cb5679cf063d9064b8bc493929
Luka Chubinidze <chubinidzel08@gmail.com>
2024-09-15 14:02:44 +0400

    add price range modal UI; add controlled input

--- src/components/all_listings_page/filter/filter_options/price/ChoosePrice.tsx
+++ src/components/all_listings_page/filter/filter_options/price/ChoosePrice.tsx
@@ -3,12 +3,25 @@
 import { useState } from "react";
 import DropDownButton from "../../../../ui/buttons/DropDownButton";
 import FilterOptionsModal from "../../../../ui/modals/FilterOptionsModal";
+import PriceInputs from "./PriceInputs";
 
 export default function ChoosePrice() {
   const [isOpen, setIsOpen] = useState(false);
 
+  const handleSubmition = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const data = new FormData(e.currentTarget);
+
+    setIsOpen(false);
+    console.log({
+      from: data.get("from"),
+      till: data.get("till"),
+    });
+  };
+
   return (
-    <div className="relative">
+    <form onSubmit={handleSubmition} className="relative">
       <DropDownButton
         label="საფასო კატეგორია"
         action={() => setIsOpen((prev) => !prev)}
@@ -16,11 +29,25 @@ export default function ChoosePrice() {
       />
       {isOpen && (
         <FilterOptionsModal label="ფასის მიხედვით">
-          <h1>
-            testtesttesttesttesttesttesttesttesttesttsttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttestv
-          </h1>
+          <div className="w-full flex flex-col justify-center items-center gap-6">
+            <PriceInputs />
+            <div className="w-full flex justify-start items-center gap-6">
+              <div className="w-full">
+                <h1 className="font-medium text-[14px]">მინ. ფასი</h1>
+                <h1 className="font-normal text-[14px]">10,000 ₾</h1>
+                <h1 className="font-normal text-[14px]">25,000 ₾</h1>
+                <h1 className="font-normal text-[14px]">35,000 ₾</h1>
+              </div>
+              <div className="w-full">
+                <h1 className="font-medium text-[14px]">მაქს. ფასი</h1>
+                <h1 className="font-normal text-[14px]">20,000 ₾</h1>
+                <h1 className="font-normal text-[14px]">30,000 ₾</h1>
+                <h1 className="font-normal text-[14px]">40,000 ₾</h1>
+              </div>
+            </div>
+          </div>
         </FilterOptionsModal>
       )}
-    </div>
+    </form>
   );
 }
