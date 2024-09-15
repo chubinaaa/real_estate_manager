3ee55487b782d21c712811c210809f5323778ce2
Luka Chubinidze <chubinidzel08@gmail.com>
2024-09-15 14:23:16 +0400

    add area filtering modal UI and inputs

---
+++ src/components/all_listings_page/filter/filter_options/area/AreaInputs.tsx
@@ -0,0 +1,19 @@
+import AreaInput from "../../../../ui/inputs/AreaInput";
+
+export default function AreaInputs() {
+  const validity = false;
+
+  return (
+    <div className="w-full flex flex-col justify-center items-start">
+      <div className="w-[325px] flex justify-center items-center gap-6">
+        <AreaInput placeholder="დან" name="from" id="from" />
+        <AreaInput placeholder="მდე" name="till" id="till" />
+      </div>
+      {validity && (
+        <h1 className="text-primaryRed font-normal text-[12px]">
+          შეიყვანეთ ვალიდური მონაცემები
+        </h1>
+      )}
+    </div>
+  );
+}
--- src/components/all_listings_page/filter/filter_options/area/ChooseArea.tsx
+++ src/components/all_listings_page/filter/filter_options/area/ChooseArea.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import DropDownButton from "../../../../ui/buttons/DropDownButton";
 import FilterOptionsModal from "../../../../ui/modals/FilterOptionsModal";
+import AreaInputs from "./AreaInputs";
 
 export default function ChooseArea() {
   const [isOpen, setIsOpen] = useState(false);
@@ -16,9 +17,39 @@ export default function ChooseArea() {
       />
       {isOpen && (
         <FilterOptionsModal label="ფართობის მიხედვით">
-          <h1>
-            testtesttesttesttesttesttesttesttesttesttsttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttestv
-          </h1>
+          <div className="w-full flex flex-col justify-center items-center gap-6">
+            <AreaInputs />
+            <div className="w-full flex justify-start items-center gap-6">
+              <div className="w-full">
+                <h1 className="font-medium text-[14px]">
+                  მინ. მ<sup>2</sup>
+                </h1>
+                <h1 className="font-normal text-[14px]">
+                  20 მ<sup>2</sup>
+                </h1>
+                <h1 className="font-normal text-[14px]">
+                  40 მ<sup>2</sup>
+                </h1>
+                <h1 className="font-normal text-[14px]">
+                  60 მ<sup>2</sup>
+                </h1>
+              </div>
+              <div className="w-full">
+                <h1 className="font-medium text-[14px]">
+                  მაქს. მ<sup>2</sup>
+                </h1>
+                <h1 className="font-normal text-[14px]">
+                  30 მ<sup>2</sup>
+                </h1>
+                <h1 className="font-normal text-[14px]">
+                  50 მ<sup>2</sup>
+                </h1>
+                <h1 className="font-normal text-[14px]">
+                  70 მ<sup>2</sup>
+                </h1>
+              </div>
+            </div>
+          </div>
         </FilterOptionsModal>
       )}
     </div>
