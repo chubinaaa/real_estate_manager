620d01a1bb298249c0169115e0622da8574f9b44
Luka Chubinidze <chubinidzel08@gmail.com>
2024-09-19 02:23:50 +0400

    add modular function to get all regions; used in filter component

--- src/components/all_listings_page/filter/filter_options/FilterOptions.tsx
+++ src/components/all_listings_page/filter/filter_options/FilterOptions.tsx
@@ -1,12 +1,15 @@
+import { getRegionList } from "../../../../utils/get_functions";
 import ChooseArea from "./area/ChooseArea";
 import ChoosePrice from "./price/ChoosePrice";
 import ChooseRegion from "./region/ChooseRegion";
 import ChooseBedrooms from "./room/ChooseRoomsCount";
 
-export default function FilterOptions() {
+export default async function FilterOptions() {
+  const regions = await getRegionList();
+
   return (
     <div className="relative w-fit h-[47px] flex justify-center items-center gap-6 px-[6px] border-[1px] border-thirdGray rounded-[10px]">
-      <ChooseRegion />
+      <ChooseRegion data={regions} />
       <ChoosePrice />
       <ChooseArea />
       <ChooseBedrooms />
---
+++ src/utils/get_functions.ts
@@ -0,0 +1,20 @@
+import { API_DOMAIN, API_TOKEN } from "./constants";
+
+export const getRegionList = async () => {
+    try {
+        const res = await fetch(`${API_DOMAIN}/regions`, {
+            method: "GET",
+            headers: {
+                "Content-type": "application/json",
+                Accept: "application/json",
+                authorization: `Bearer ${API_TOKEN}`,
+            },
+        });
+        if (!res.ok) throw new Error("unable to fetch regions list");
+
+        return await res.json() as Array<Region>;
+    } catch (error) {
+        console.error(error)
+        return null
+    }
+};
