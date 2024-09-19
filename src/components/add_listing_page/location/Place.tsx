d269b93f8da3b1e5c90e70dc9834c6e228726caf
Luka Chubinidze <chubinidzel08@gmail.com>
2024-09-19 12:03:22 +0400

    add pure/reusable custom select component and used in estate upload page; component is accessible in FormData

--- src/components/add_listing_page/location/Place.tsx
+++ src/components/add_listing_page/location/Place.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import SelectOptions from "../../ui/selections/SelectOptions";
 
 type Props = {
@@ -9,28 +9,28 @@ type Props = {
 };
 
 export default function Place({ cities, regions }: Props) {
-  const regionState = useState({ id: 0, name: "" });
-  const cityState = useState({ id: 0, name: "" });
-
-  useEffect(() => {
-    cityState[1]({ id: 0, name: "" });
-  }, [regionState[0].id]);
+  const regionState = useState(0);
+  const cityState = useState(0);
 
   return (
     <div className="w-full grid grid-cols-2 gap-x-5">
       {regions && (
         <SelectOptions
+          id="estate_upload_region_selection"
+          name="region_id"
           labelText="რეგიონი"
           options={regions}
           state={regionState}
         />
       )}
-      {cities && regionState[0].name.trim().length > 0 && (
+      {cities && regionState[0] !== 0 && (
         <SelectOptions
+          id="estate_upload_city_selection"
+          name="city_id"
           labelText="ქალაქი"
-          options={cities.filter(
-            (city) => city.region_id === regionState[0].id
-          )}
+          options={cities
+            .filter((city) => city.region_id === regionState[0])
+            .map((city) => ({ id: city.id, name: city.name }))}
           state={cityState}
         />
       )}
--- src/components/ui/selections/SelectOptions.tsx
+++ src/components/ui/selections/SelectOptions.tsx
@@ -1,16 +1,14 @@
 "use client";
 
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Select from "./Select";
 import DropDownModal from "./DropDownModal";
 import Option from "./Option";
 
-type OptionType = { id: number; name: string };
-
 type Props = React.InputHTMLAttributes<HTMLSelectElement> & {
   labelText: string;
-  options: Array<OptionType>;
-  state: [OptionType, React.Dispatch<React.SetStateAction<OptionType>>];
+  options: Array<{ id: number; name: string }>;
+  state: [number, React.Dispatch<React.SetStateAction<number>>];
 };
 
 export default function SelectOptions({
@@ -19,33 +17,54 @@ export default function SelectOptions({
   state,
   ...props
 }: Props) {
+  const selectionRef = useRef<HTMLSelectElement>(null);
   const modal = useState(false);
-  const [selectedValue, setSelectedValue] = state;
+  const [selectedId, setSelectedId] = state;
 
-  const handleSelection = (option: OptionType) => {
-    setSelectedValue(option);
+  const handleSelection = (option: number) => {
+    setSelectedId(option);
+    if (selectionRef.current) selectionRef.current.value = option.toString();
 
     // Close modal after selection
     modal[1](false);
   };
 
   return (
-    <div className="relative w-full flex flex-col justify-center items-start gap-[5px] | select-none">
-      <label htmlFor={props.id} className="text-[14px] font-medium">
-        {labelText}
-      </label>
-      <Select selectedName={selectedValue.name} state={modal} />
-      {modal[0] && (
-        <DropDownModal>
-          {options.map((opt) => (
-            <Option
-              key={opt.id}
-              name={opt.name}
-              onClick={() => handleSelection(opt)}
-            />
-          ))}
-        </DropDownModal>
-      )}
-    </div>
+    <>
+      <select
+        hidden
+        ref={selectionRef}
+        value={selectedId}
+        onChange={(e) => setSelectedId(Number(e.target.value))}
+        {...props}
+      >
+        <option key={0} value={0}></option>
+        {options.map((opt) => (
+          <option key={opt.id} value={opt.id}>
+            {opt.name}
+          </option>
+        ))}
+      </select>
+      <div className="relative w-full flex flex-col justify-center items-start gap-[5px] | select-none">
+        <label htmlFor={props.id} className="text-[14px] font-medium">
+          {labelText}
+        </label>
+        <Select
+          selectedName={options.find((el) => el.id === selectedId)?.name || ""}
+          state={modal}
+        />
+        {modal[0] && (
+          <DropDownModal>
+            {options.map((opt) => (
+              <Option
+                key={opt.id}
+                name={opt.name}
+                onClick={() => handleSelection(opt.id)}
+              />
+            ))}
+          </DropDownModal>
+        )}
+      </div>
+    </>
   );
 }
