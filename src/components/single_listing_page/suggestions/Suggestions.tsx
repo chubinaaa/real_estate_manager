76655c191e187f98cd1510aef61d27ec8628d6ec
Luka Chubinidze <chubinidzel08@gmail.com>
2024-09-21 20:35:10 +0400

    add carousel scroll logic

--- src/components/single_listing_page/suggestions/Suggestions.tsx
+++ src/components/single_listing_page/suggestions/Suggestions.tsx
@@ -1,3 +1,4 @@
+import { getAllEstates } from "../../../utils/get_functions";
 import Carousel from "./carousel/Carousel";
 
 type Props = {
@@ -5,13 +6,16 @@ type Props = {
   estateRegionId: number;
 };
 
-export default function Suggestions({ estateId, estateRegionId }: Props) {
+export default async function Suggestions({ estateId, estateRegionId }: Props) {
+  const data = await getAllEstates();
+
   return (
     <div className="w-full flex flex-col justify-center items-start mt-[67px] gap-12 mb-[250px]">
       <h1 className="font-medium text-[32px]">ბინები მსგავს ლოკაციაზე</h1>
       <Carousel
         excitingListingId={estateId}
         excitingRegionId={estateRegionId}
+        data={data || []}
       />
     </div>
   );
